e7cc2ca69ca0146a9ce22aa4e5b2fc77866227c0
Parv Agarwal <65726543+Parvfect@users.noreply.github.com>
2024-08-05 10:29:30 +0100

    Update training_loop.py

--- training_loop.py
+++ training_loop.py
@@ -50,7 +50,7 @@ if saved_model:
 
 
 
-X, y = data_preproc()
+X, y = data_preproc(chop_reads=0.1)
 
 # Creating Train, Test, Validation sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
